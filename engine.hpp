1ff04340c0f3398d8966319d97e91a24e5704be3
Michael Mahoney <michael.mahoney@alphaomega.com.co>
2025-08-10 23:19:04 -0600

    push for the day

--- engine.hpp
+++ engine.hpp
@@ -5,16 +5,77 @@
 #ifndef ENGINE_HPP
 #define ENGINE_HPP
 
+struct coordinates {
+    float x;
+    float y;
+    float z;
+};
+
+enum Action_Type {
+    Move,
+    Standard,
+    Swift,
+    Immediate,
+    Free,
+};
+
+class Action {
+
+    public:
+        Action();
+        ~Action();
+        
+        Trigger* run_action(Action* self, Action* other_actor);
+    
+    private:
+       std::string stat_name;
+       Error set_stat(int* stat);
+       int stat;
+       
+       std::string check_name;
+       Error set_check(int* stat);
+       int check;
+
+
+        
+    
+};
+
+class Trigger {
+
+};
+
 enum Error {
     GeneralError
 };
 
-enum Action 
-{
-    Attack,
-    Defend,
-    Ready_Action,
-    Skill_Check
+// enum AcVtion 
+// {
+//     Attack,
+//     Defend,
+//     Ready_Action,
+//     Skill_Check
+// };
+
+class Actor {
+    public:
+        Actor();
+        ~Actor();
+        
+        Error move(coordinates coord);
+        coordinates get_location();
+        float get_distance_to_actor(Actor actor);
+
+    
+    private:
+        coordinates location;
+
+
+
+};
+
+class Character : public Actor {
+
 };
 
 class Engine 
@@ -28,6 +89,10 @@ class Engine
         
     
     private:
+        int round;
+        Character* whos_turn;
+        Character* characters[];
+
     
 
     
@@ -35,22 +100,5 @@ class Engine
 
 };
 
-int engine()
-{
-    std::string name;
-    std::string dump;
-    double number;
-
-    std::cout << "Enter you name: ";
-    std::cin >> name;
-    
-    std::string phrase = "Hello, " + name;
-    std::cout << phrase << std::endl;
-    
-    std::cin >> dump;
-    
-
-    return 0;
-}
 
 #endif
